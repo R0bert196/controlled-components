0af3acebe09aa5b6e1a52dfa9e4e06d60445a6ba
R0bert196 <robertchiriac196@gmail.com>
2022-08-25 00:36:42 +0300

    Complete uncontrolled component

--- src/Uncontrolled.jsx
+++ src/Uncontrolled.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import { useRef, useState } from "react";
 
 function Uncontrolled() {
+    const name = useRef("robert");
+    const skill = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const developerName = name.current.value;
+    const desctiption = document.querySelector("#desctiption").value;
+    console.log("name: " + developerName);
+    console.log("desctiption: " + desctiption);
+    console.log("Limbaj preferat: " + getRadioValue().value)
+    console.log("Skill: " +  skill.current.value)
+  };
+    
+    
+    const getRadioValue  = () => {
+      const elements = document.getElementsByName("fav-language");
+
+      for (let i = 0; i < elements.length; i++) {
+          if (elements[i].checked) {
+              return elements[i];
+          }
+      }
+    }
+
   return (
-      <div>
-          <form className='form'>
-              <input
-                  type="text"
-                  required
-              />
-              <textarea
-                  required
-              />
-              <select>
-                  <option value="1">robert</option>
-                  <option value="2">alex</option>
-              </select>
-              <button type='submit'>Continua</button>
-          </form>  
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        {/* Folosind ref  */}
+        <input ref={name} placeholder='Nume developer' type='text' required />
+        {/* Folosind queySelector */}
+        <textarea id='desctiption' placeholder='Aptitudini' required />
+              
+        <select ref={skill}>
+          <option value='junior'>junior</option>
+          <option value='senior'>senior</option>
+        </select>
+        {/* In cazul radio buttons */}
+        <div>
+          <div>
+            <label>Java</label>
+            <input type='radio' name='fav-language' value='java' />
+          </div>
+          <div>
+            <label>C#</label>
+            <input type='radio' name='fav-language' value='c#' />
+          </div>
+          <div>
+            <label>JS</label>
+            <input type='radio' name='fav-language' value='JS' />
+          </div>
+        </div>
+
+        <button type='submit'>Continua</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Uncontrolled
+export default Uncontrolled;
